2882e310fbcd1686da3c58bf7a775e1b7bb9154c
sammy33440-commits <sammy33440@gmail.com>
2026-01-25 20:34:29 +0100

    Update build_waveshare_rp2350_usb_a.sh

--- src/boards/build_waveshare_rp2350_usb_a.sh
+++ src/boards/build_waveshare_rp2350_usb_a.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cmake -G "Unix Makefiles" -DFAMILY=rp2350 -DPICO_BOARD=waveshare_rp2350_usb_a -B build
+cmake -G "Unix Makefiles" -DFAMILY=rp2350 -DPICO_BOARD=waveshare_rp2350_usb_a -DPICO_USB_HOST_DP_PIN=28 -DPICO_USB_HOST_DM_PIN=29 -DPICO_DEFAULT_LED_PIN=-1 -B build
