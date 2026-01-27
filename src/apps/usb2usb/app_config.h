f4f54c24463746d664d770a5fccfe1d2770c6320
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 14:11:19 +0100

    Update app_config.h

--- src/apps/usb2usb/app_config.h
+++ src/apps/usb2usb/app_config.h
@@ -1,11 +1,14 @@
 #ifndef _APP_CONFIG_H_
 #define _APP_CONFIG_H_
 
-// Active le mode USB vers USB
+#include "input/usb_input.h"
+#include "output/usb_output.h"
+
+// Active le pont USB entre les deux ports
 #define ENABLE_USB_HOST
 #define ENABLE_USB_DEVICE
 
-// On ne met PAS d'include ici, Joypad-OS les gère ailleurs
+// Définit les interfaces (obligatoire pour router.c)
 #define APP_INPUT_INTERFACES  &input_usb
 #define APP_OUTPUT_INTERFACES &output_usb
 
