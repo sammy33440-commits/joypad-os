36f5500be0e4c303bb0c492f7350141368f1b3c4
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 13:55:33 +0100

    Update app_config.h

--- src/apps/usb2usb/app_config.h
+++ src/apps/usb2usb/app_config.h
@@ -1,15 +1,17 @@
 #ifndef _APP_CONFIG_H_
 #define _APP_CONFIG_H_
 
-// Force l'utilisation des deux interfaces USB
+// On ajoute les "dictionnaires" pour que le code comprenne input_usb
+#include "input/usb_input.h"
+#include "output/usb_output.h"
+
 #define ENABLE_USB_HOST
 #define ENABLE_USB_DEVICE
 
-// Définit les interfaces d'entrée et de sortie
+// On définit qui est l'entrée et qui est la sortie
 #define APP_INPUT_INTERFACES  &input_usb
 #define APP_OUTPUT_INTERFACES &output_usb
 
-// Configuration du polling (vitesse de lecture)
 #define APP_POLL_INTERVAL_MS 1
 
 #endif
