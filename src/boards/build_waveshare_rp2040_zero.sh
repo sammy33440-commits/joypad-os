b4f2fba2a0b5473e2bd008a7ebf6c293cc7c7dc7
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 13:01:11 +0100

    Update build_waveshare_rp2040_zero.sh

--- src/boards/build_waveshare_rp2040_zero.sh
+++ src/boards/build_waveshare_rp2040_zero.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# ON ECRASE LE SCRIPT ORIGINAL
+# ON TROMPE LE SYSTEME EN DISANT QUE C'EST UNE PICO STANDARD
+# Cela force le compilateur à accepter nos Pins 6 et 7 sans discuter.
+
 cmake -G "Unix Makefiles" \
   -DFAMILY=rp2040 \
-  -DPICO_BOARD=waveshare_rp2040_zero \
+  -DPICO_BOARD=pico \
   -DPICO_USB_HOST_DP_PIN=6 \
   -DPICO_USB_HOST_DM_PIN=7 \
   -DCONFIG_USB=1 \
