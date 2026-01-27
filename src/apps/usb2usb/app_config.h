dd2fb80234a2e482994673b759658060ef1efef7
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 14:04:34 +0100

    Update app_config.h

--- src/apps/usb2usb/app_config.h
+++ src/apps/usb2usb/app_config.h
@@ -1,14 +1,11 @@
 #ifndef _APP_CONFIG_H_
 #define _APP_CONFIG_H_
 
-// On ajoute les "dictionnaires" pour que le code comprenne input_usb
-#include "input/usb_input.h"
-#include "output/usb_output.h"
-
+// Active le mode USB vers USB
 #define ENABLE_USB_HOST
 #define ENABLE_USB_DEVICE
 
-// On définit qui est l'entrée et qui est la sortie
+// On ne met PAS d'include ici, Joypad-OS les gère ailleurs
 #define APP_INPUT_INTERFACES  &input_usb
 #define APP_OUTPUT_INTERFACES &output_usb
 
