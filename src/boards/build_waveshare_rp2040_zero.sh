a0426cfaed13ee5c36b88ceb348aec5fd2c2f115
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 13:21:40 +0100

    Update build_waveshare_rp2040_zero.sh

--- src/boards/build_waveshare_rp2040_zero.sh
+++ src/boards/build_waveshare_rp2040_zero.sh
@@ -2,7 +2,7 @@
 cmake -G "Unix Makefiles" \
   -DFAMILY=rp2040 \
   -DPICO_BOARD=waveshare_rp2040_zero \
-  -DPICO_USB_HOST_DP_PIN=7 \
-  -DPICO_USB_HOST_DM_PIN=6 \
+  -DPICO_USB_HOST_DP_PIN=6 \
+  -DPICO_USB_HOST_DM_PIN=7 \
   -DCONFIG_USB=1 \
   -B build
