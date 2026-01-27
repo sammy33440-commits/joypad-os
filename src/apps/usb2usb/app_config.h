91e0b258b3212d126ddc11aa798e1f3915c0dcca
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 14:35:24 +0100

    Update app_config.h

--- src/apps/usb2usb/app_config.h
+++ src/apps/usb2usb/app_config.h
@@ -1,14 +1,10 @@
 #ifndef _APP_CONFIG_H_
 #define _APP_CONFIG_H_
 
-#include "input/usb_input.h"
-#include "output/usb_output.h"
-
-// Active le pont USB entre les deux ports
+// On laisse le système inclure les fichiers lui-même
 #define ENABLE_USB_HOST
 #define ENABLE_USB_DEVICE
 
-// Définit les interfaces (obligatoire pour router.c)
 #define APP_INPUT_INTERFACES  &input_usb
 #define APP_OUTPUT_INTERFACES &output_usb
 
