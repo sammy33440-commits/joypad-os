a400a01ffb86346be772fcdb40daab8449b9202d
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 13:24:38 +0100

    Update build_waveshare_rp2040_zero.sh

--- src/boards/build_waveshare_rp2040_zero.sh
+++ src/boards/build_waveshare_rp2040_zero.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+# SOLUTION ULTIME : FORCE 6/7 + DISABLE VBUS CHECK
+# On utilise PICO_BOARD=pico pour éviter les surcouches Waveshare
+# On force -1 sur VBUS pour que le driver démarre sans attendre
+
 cmake -G "Unix Makefiles" \
   -DFAMILY=rp2040 \
-  -DPICO_BOARD=waveshare_rp2040_zero \
+  -DPICO_BOARD=pico \
   -DPICO_USB_HOST_DP_PIN=6 \
   -DPICO_USB_HOST_DM_PIN=7 \
+  -DPIO_USB_DP_PIN=6 \
+  -DPIO_USB_VBUS_PIN=-1 \
   -DCONFIG_USB=1 \
   -B build
