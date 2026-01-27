632fdcaad91fde3aab43f0f75ea2bb0d5be4adea
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 14:00:23 +0100

    Update tusb_config.h

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -1,39 +1,40 @@
 #ifndef _TUSB_CONFIG_H_
 #define _TUSB_CONFIG_H_
 
-// Dictionnaire de base
 #include "tusb_option.h"
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
+// Configuration de base RP2040
 #define CFG_TUSB_MCU                OPT_MCU_RP2040
 #define CFG_TUSB_OS                 OPT_OS_NONE
+#define CFG_TUSB_DEBUG              0
 
-// Port DROITE = PC | Port GAUCHE = Manette
+// CONFIGURATION DES PORTS (6/7 pour PiZero)
 #define CFG_TUSB_RHPORT0_MODE       OPT_MODE_DEVICE
-#define CFG_TUSB_RHPORT1_MODE       OPT_MODE_HOST
+#define CFG_TUSB_RHPORT1_MODE       (OPT_MODE_HOST | OPT_MODE_FULL_SPEED)
 #define CFG_TUH_RPI_PIO_USB         1
 
-// Pins 6 et 7 pour Waveshare PiZero
 #define PICO_USB_HOST_DP_PIN        6
 #define PICO_USB_HOST_DM_PIN        7
 
-// Config Host (Entrée)
+// CONFIGURATION HOST (MANETTE)
+#define CFG_TUH_ENABLED             1
 #define CFG_TUH_HUB                 1
-#define CFG_TUH_HID                 8
+#define CFG_TUH_HID                 4
 #define CFG_TUH_XINPUT              4
-#define CFG_TUH_DEVICE_MAX          (4*CFG_TUH_HUB + 1)
-#define CFG_TUH_ENUMERATION_BUFSIZE 1280
+#define CFG_TUH_DEVICE_MAX          (CFG_TUH_HUB ? 5 : 1)
+#define CFG_TUH_ENUMERATION_BUFSIZE 256
 
-// Config Device (Sortie)
-#define CFG_TUD_HID                 4
-#define CFG_TUD_XID                 1
-#define CFG_TUD_XINPUT              1
-#define CFG_TUD_GC_ADAPTER          1
-#define CFG_TUD_CDC                 1
+// CONFIGURATION DEVICE (CONSOLE)
+#define CFG_TUD_ENABLED             1
+#define CFG_TUD_MAX_SPEED           OPT_MODE_FULL_SPEED
 #define CFG_TUD_ENDPOINT0_SIZE      64
+#define CFG_TUD_HID                 1
+#define CFG_TUD_CDC                 1
+#define CFG_TUD_XINPUT              1
 
 #ifdef __cplusplus
  }
